9f550dbab530c562e9232c06627c86a883ddf735
macarval <macarval@student.42sp.org.br>
2024-06-03 15:16:35 -0300

    [Inception]: Correction

--- srcs/requirements/bonus/adminer/tools/entrypoint.sh
+++ srcs/requirements/bonus/adminer/tools/entrypoint.sh
@@ -2,7 +2,7 @@
 
 # Set up php-fpm
 mkdir -p /var/run/php
-sed -i 's/listen = \/run\/php\/php7.4-fpm.sock/listen = 0.0.0.0:9001/g' /etc/php/7.4/fpm/pool.d/www.conf
+sed -i 's/listen = \/run\/php\/php7.4-fpm.sock/listen = 0.0.0.0:9000/g' /etc/php/7.4/fpm/pool.d/www.conf
 
 # Set up adminer
 mkdir -p /var/www/html
@@ -11,4 +11,4 @@ chown www-data:www-data /var/www/html/adminer.php
 chmod 755 /var/www/html/adminer.php
 
 # Start php-fpm
-# php-fpm -F
+php-fpm -F
