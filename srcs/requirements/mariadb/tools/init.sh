661b27df64d2c09571625726dfef5f66378e9b92
macarval <macarval@student.42sp.org.br>
2024-06-05 16:15:12 -0300

    [Inception]: Correction

--- srcs/requirements/mariadb/tools/init.sh
+++ srcs/requirements/mariadb/tools/init.sh
@@ -1,27 +1,3 @@
-# #!/bin/bash
-
-# service mariadb start
-
-# # Create and import WordPress database
-# mariadb -u root -e "CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE;"
-# mariadb -u root -e "CREATE USER IF NOT EXISTS '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';"
-# mariadb -u root -e "GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%';"
-# mariadb -u root -e "FLUSH PRIVILEGES;"
-
-# # Import database dump
-# mariadb -u root $MYSQL_DATABASE < /usr/local/bin/dump.sql
-
-# # Set password for root user
-# mariadb -u root -e "SET PASSWORD FOR 'root'@'localhost' = PASSWORD('$MYSQL_ROOT_PASSWORD');"
-# mariadb -u root -e "FLUSH PRIVILEGES;"
-
-# # Allow root user to login from any host
-# mariadb -u root -p $MYSQL_ROOT_PASSWORD "GRANT ALL ON *.* TO 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';"
-# mariadb -u root -p $MYSQL_ROOT_PASSWORD "FLUSH PRIVILEGES;"
-
-# service mariadb stop
-
-
 #!/bin/bash
 
 service mariadb start
@@ -35,14 +11,19 @@ mariadb -u root -e "FLUSH PRIVILEGES;"
 # Import database dump
 mariadb -u root $MYSQL_DATABASE < /usr/local/bin/dump.sql
 
+# # Set password for root user
+# mariadb -u root -e "SET PASSWORD FOR 'root'@'localhost' = PASSWORD('$MYSQL_ROOT_PASSWORD');"
+# mariadb -u root -e "FLUSH PRIVILEGES;"
+
+# # Allow root user to login from any host
+# mariadb -u root -p $MYSQL_ROOT_PASSWORD "GRANT ALL ON *.* TO 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';"
+# mariadb -u root -p $MYSQL_ROOT_PASSWORD "FLUSH PRIVILEGES;"
+
+#
 # Set password for root user
 mariadb -u root -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';"
 mariadb -u root -e "ALTER USER 'root'@'%' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';"
 mariadb -u root -e "FLUSH PRIVILEGES;"
-
-# Allow root user to login from any host with the specified password
-mariadb -u root -e "GRANT ALL PRIVILEGES ON *.* TO 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD' WITH GRANT OPTION;"
-mariadb -u root -e "GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD' WITH GRANT OPTION;"
-mariadb -u root -e "FLUSH PRIVILEGES;"
+#
 
 service mariadb stop
